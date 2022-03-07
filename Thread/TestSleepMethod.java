d3dd1b60361e6e5091e1ac9a494f37a4ea9a326b
RashmiKata <97656103+RashmiKata@users.noreply.github.com>
2022-03-07 16:55:11 +0530

    Add files via upload

--- Thread/TestSleepMethod.java
+++ Thread/TestSleepMethod.java
@@ -1,6 +1,8 @@
 package ThreadExample;
 
+
 public class TestSleepMethod extends Thread{
+
 
 	public void run()
 	{
@@ -14,16 +16,26 @@ public class TestSleepMethod extends Thread{
 			{
 				System.out.println(e);
 			}
-			System.out.println(i);
+			//System.out.println(i);
+			System.out.println(Thread.currentThread().getName()+" = "+ i);
 		}
 	}
 	
 	public static void main(String[] args) {
 		TestSleepMethod t1 = new TestSleepMethod();
 		TestSleepMethod t2 = new TestSleepMethod();
+		TestSleepMethod t3= new TestSleepMethod();
 		
 		t1.start();
+		try {
+			t1.join(1500);
+		}
+		catch (InterruptedException ee){
+			System.out.println(ee);
+		}//t1.start();//IllegalThreadStateException
+		
 		t2.start();
+		t3.start();
 	}
 }
 /*
@@ -40,4 +52,24 @@ output
 4
 5
 5
+*/
+/*
+Thread-0 = 0
+Thread-0 = 1
+Thread-0 = 2
+Thread-1 = 0
+Thread-2 = 0
+Thread-0 = 3
+Thread-1 = 1
+Thread-2 = 1
+Thread-0 = 4
+Thread-1 = 2
+Thread-2 = 2
+Thread-0 = 5
+Thread-1 = 3
+Thread-2 = 3
+Thread-1 = 4
+Thread-2 = 4
+Thread-1 = 5
+Thread-2 = 5
 */
