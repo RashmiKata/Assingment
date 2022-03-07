e248e8d44db935237c4401163f0ae08826741534
RashmiKata <97656103+RashmiKata@users.noreply.github.com>
2022-03-07 14:32:10 +0530

    Add files via upload

---
+++ Thread/TestSleepMethod.java
@@ -0,0 +1,43 @@
+package ThreadExample;
+
+public class TestSleepMethod extends Thread{
+
+	public void run()
+	{
+		for(int i=0; i<=5; i++)
+		{
+			try 
+			{
+				Thread.sleep(500);
+			}
+			catch(InterruptedException e)
+			{
+				System.out.println(e);
+			}
+			System.out.println(i);
+		}
+	}
+	
+	public static void main(String[] args) {
+		TestSleepMethod t1 = new TestSleepMethod();
+		TestSleepMethod t2 = new TestSleepMethod();
+		
+		t1.start();
+		t2.start();
+	}
+}
+/*
+output
+0
+0
+1
+1
+2
+2
+3
+3
+4
+4
+5
+5
+*/
---
+++ Thread/ThreadDemo.java
@@ -0,0 +1,45 @@
+package ThreadExample;
+
+public class ThreadDemo extends Thread {
+
+	public void run()
+	{
+		System.out.println("Thread is running...");
+		System.out.println("Thread priority: "+Thread.currentThread());
+	}
+	
+	public static void main(String[] args) {
+		ThreadDemo t1 = new ThreadDemo();	//new state
+		ThreadDemo t2 = new ThreadDemo();
+		
+		t1.start(); //runnable state
+		t2.start();
+	
+		//getting the thread name by invoking the getName(
+		String n1 = t1.getName();
+		System.out.println("t1 name: "+n1);
+		
+		String n2 = t2.getName();
+		System.out.println("t2 name: "+n2);
+		t1.setName("First thread");
+	
+		String change = t1.getName();
+		System.out.println("t1 name: "+change);
+	
+		System.out.println(t1.getPriority());
+		System.out.println(t2.getPriority());
+		
+		 t2.setPriority(Thread.MAX_PRIORITY);
+		 t1.setPriority(Thread.MAX_PRIORITY);
+	}
+}
+/*
+output
+Thread priority: Thread[Thread-1,5,main]
+t1 name: Thread-0
+t2 name: Thread-1
+t1 name: First thread
+Thread priority: Thread[Thread-0,5,main]
+5
+5
+*/
